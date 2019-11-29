f9fb817d77acc6cb1b673d246aed0afbc8e4f9b4
tomkf <thomaskeithferris@gmail.com>
2019-11-29 15:08:47 -0800

    Add project files.

---
+++ cityVancouver_API/APIModels.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace cityVancouver_API
+{
+    public class FoodCartContext : DbContext
+    {
+        //will need connection string 
+
+        String connectionString = "Server=.;Database=cityOfVacouverApi;Trusted_Connection=True;";
+
+        public DbSet<Rootobject> Root { get; set; }
+
+        // this sets up connection string 
+        // add migration initial create 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(connectionString);
+        }
+    }
+
+    public class FoodCart
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string status { get; set; }
+        public string description { get; set; }
+        public string geo_localarea { get; set; }
+        public decimal longitude { get; set; }
+        public decimal latitude { get; set; }
+        public string location { get; set; }
+        public string key { get; set; }
+        public string vendor_type { get; set; }
+        public string business_name { get; set; }
+    }
+
+    public class Rootobject 
+         {
+            public int nhits { get; set; }
+            public Parameters parameters { get; set; }
+            public Record[] records { get; set; }
+        }
+
+        public class Parameters
+        {
+            public string dataset { get; set; }
+            public string timezone { get; set; }
+            public int rows { get; set; }
+            public string format { get; set; }
+        }
+
+        public class Record
+        {
+            public string datasetid { get; set; }
+            public string recordid { get; set; }
+            public Fields fields { get; set; }
+            public DateTime record_timestamp { get; set; }
+        }
+
+        public class Fields
+        {
+            public string status { get; set; }
+            public string description { get; set; }
+            public string geo_localarea { get; set; }
+            public Geom geom { get; set; }
+            public string location { get; set; }
+            public string key { get; set; }
+            public string vendor_type { get; set; }
+            public string business_name { get; set; }
+        }
+
+        public class Geom
+        {
+            public string type { get; set; }
+            public float[] coordinates { get; set; }
+        }
+}
---
+++ cityVancouver_API/Program.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace cityVancouver_API
+{
+    class Program
+    {  
+
+        static void Main(string[] args)
+        {
+            const string protocol = "https";
+            const string baseURL = "://opendata.vancouver.ca/api/records/1.0/search/?dataset=food-vendors&rows=1000";
+
+            Uri uri = new Uri(protocol + baseURL);
+
+            string jsonString = CallRestMethod(uri);
+
+            Rootobject rootObject = JsonConvert.DeserializeObject<Rootobject>(jsonString);
+
+            FoodCartContext db = new FoodCartContext();
+
+            foreach (Record cart in rootObject.records)
+            {
+                db.Add(new FoodCart());
+            }
+        }
+
+        static string CallRestMethod(Uri uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            // Get the web response from the api
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            // Get a stream to read the reponse
+            StreamReader responseStream = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            // Read the response and write it to the console
+            // Close the connection to the api and the stream reader
+            string stringResponce =  responseStream.ReadToEnd();
+            response.Close();
+            responseStream.Close();
+            return stringResponce;
+        }
+
+
+        static class SqlHelper
+        {
+            public static SqlDataReader ExecuteReader(String connString, String commandText)
+            {
+                SqlConnection conn = new SqlConnection(connString);
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                    return reader;
+                }
+            }
+        }
+    }
+}
